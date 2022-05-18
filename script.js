86fcdf3140b4ea1c6b36a1db5e8489653d5c57fc
@LISAN LE <105549298+Lisanle1@users.noreply.github.com>
2022-05-18 19:06:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+let myResume={
+    "basics": {
+      "name": "Lisan M",
+      "email": "xyz@gmail.com",
+      "phone": 1234567890,
+      "degree": "B.E",
+      "location": {
+        "address": "No.81,anna nagar 1st street",
+        "postalCode": 600102,
+        "city": "Chennai",
+        "state": "Tamilnadu"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Lisanle1"
+        }
+      ]
+    },
+    "education": [
+      {
+        "College": "Anna University",
+        "department": "Electrical and Electronics Engineering",
+        "batch start year": 2016,
+        "batch end year": 2020,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "C and JAVA",
+        "level": "Intermediate",
+      }
+    ],
+    "project": [
+          "Energy savings in radial distribution system(Wind energy)"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Full stack development,OOPS",
+      }
+    ]
+  }
+  console.log(myResume);
